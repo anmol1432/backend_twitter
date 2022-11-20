6a0dfdff8fa19081c2d64f81e94572cf3af78827
anmol <anmolbuttar143205gmail.com>
2022-11-20 11:11:12 +0530

    enabling cors

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ const db = process.env.DATABASE
 app.use(cors())
 app.use(express.json());
 app.use(require('./routes/index'))
-
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true }).then(() => {
     console.log("MONGO DB CONNECTED");
 }).then(() => {
